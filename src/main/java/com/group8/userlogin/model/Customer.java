1057add9175d5002b62fee57cbd6287f80d02240
Johanna Salmi Sunnerdahl <73692594+Salmihanna@users.noreply.github.com>
2021-10-19 15:35:07 +0200

    Alla var med och modprogrammerade

---
+++ src/main/java/com/group8/userlogin/model/Customer.java
@@ -0,0 +1,61 @@
+package com.group8.userlogin.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private int age;
+
+    public Customer() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
