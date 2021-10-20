74d9b145886551e2291a7a551fe7e53a7e6266f0
Kristian <ChristianSaomii@gmail.com>
2021-10-20 16:38:02 +0200

    Login, css, autentication, grupp arbete!

--- src/main/java/com/group8/userlogin/service/CustomerServiceImplementation.java
+++ src/main/java/com/group8/userlogin/service/CustomerServiceImplementation.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class CustomerServiceImplementation implements CustomerService{
+public class CustomerServiceImplementation implements CustomerService {
     @Autowired
     private CustomerRespository customerRespository;
 
