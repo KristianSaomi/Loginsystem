5473650f077b9e7645b57863718e3f0b0e306699
Kristian <ChristianSaomii@gmail.com>
2021-10-20 14:29:57 +0200

    Helt ny frontend home design, super rocket design.

--- src/main/java/com/group8/userlogin/controller/CustomerController.java
+++ src/main/java/com/group8/userlogin/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package com.group8.userlogin.controller;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.group8.userlogin.model.Customer;
 import com.group8.userlogin.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomerController {
@@ -34,6 +37,16 @@ public class CustomerController {
         customerService.saveCustomer(customer);
         return "redirect:/home";
     }
+
+    public class MyErrorController implements ErrorController {
+
+        @RequestMapping("/error")
+        public String handleError() {
+            return "error";
+        }
+    }
+
+
 
 
 }
