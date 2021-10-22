0d4d1ff92994e4e0a291caa9c633cc554ce4a9c7
Kristian <ChristianSaomii@gmail.com>
2021-10-22 10:17:30 +0200

    Javascript, new thank you page etc css

--- src/main/java/com/group8/userlogin/WebSecurityConfig.java
+++ src/main/java/com/group8/userlogin/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/cssAndJs/**", "/images/**", "customer.css/**", "login.css/**", "/aboutUs.css/**","/js/**","/modalAboutUs.js/**", "/thankyou.css/");
+                .antMatchers("/resources/**", "/static/**", "/cssAndJs/**", "/images/**", "customer.css/**", "login.css/**", "/aboutUs.css/**","/js/**","/modalAboutUs.js/**", "/thankyou");
     }
 
     @Bean
--- src/main/java/com/group8/userlogin/service/CustomerService.java
+++ src/main/java/com/group8/userlogin/service/CustomerService.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public interface CustomerService {
     List<Customer> getAllCustomerList();
-
     void saveCustomer(Customer customer);
+
 }
